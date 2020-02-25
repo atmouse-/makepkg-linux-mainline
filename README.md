b637a3c7e2a1400f7da7eac0594f7b6fa69b0899
jingjiezhuang <jingjie.zhuang@igg.com>
2020-02-25 21:11:22 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# makepkg-linux-mainline
---
+++ modpreload.sh
@@ -0,0 +1,115 @@
+modprobe scsi_mod
+modprobe sd_mod
+modprobe uas
+modprobe usb_storage
+modprobe joydev
+modprobe tun
+modprobe uinput
+modprobe ntfs
+modprobe fuse
+
+modprobe ccp
+modprobe wmi
+modprobe wmi_bmof
+modprobe agpgart
+modprobe i2c_dev
+
+modprobe iscsi_tcp
+modprobe scsi_transport_iscsi
+modprobe scsi_mod
+
+# ideapad 720s 13-arr
+modprobe i2c-amd-mp2-pci
+modprobe i2c-amd-mp2-plat
+
+modprobe nft_reject_ipv4
+modprobe nft_reject_ipv6
+modprobe nft_chain_nat
+modprobe nft_connlimit
+modprobe nft_counter
+modprobe nft_ct
+modprobe nft_dup_netdev
+modprobe nft_fwd_netdev
+modprobe nft_hash
+modprobe nft_limit
+modprobe nft_log
+modprobe nft_masq
+modprobe nft_nat
+modprobe nft_numgen
+modprobe nft_objref
+modprobe nft_osf
+modprobe nft_quota
+modprobe nft_redir
+modprobe nft_reject
+modprobe nft_reject_inet
+modprobe nft_socket
+modprobe nft_synproxy
+modprobe nft_tproxy
+modprobe nft_tunnel
+
+modprobe ext4
+modprobe xfs
+
+modprobe exfat
+
+# 2019-10-22
+modprobe pl2303
+
+modprobe snd_usb_audio
+modprobe snd_usbmidi_lib
+modprobe snd_rawmidi
+modprobe snd_seq_device
+modprobe mc
+
+# 2019-10-26
+modprobe cifs
+modprobe nls_utf8
+
+# 2019-11-18
+modprobe usbnet
+modprobe rtl8152
+modprobe asix
+
+# 2019-12-06
+modprobe e1000e
+
+# 2019-12-25
+modprobe nbd
+modprobe loop
+modprobe rbd
+
+# 2019-12-28
+modprobe rtl8150
+
+# 2019-12-29
+modprobe xfrm4_tunnel
+modprobe xfrm6_tunnel
+modprobe nft_xfrm
+modprobe xfrm_algo
+modprobe xfrm_interface
+modprobe xfrm_ipcomp
+modprobe xfrm_user
+modprobe mac80211
+modprobe rtl8192cu
+
+# 2020-01-03
+modprobe kvm_intel
+modprobe kvm_amd
+modprobe r8169
+modprobe realtek
+
+# 2020-01-08
+modprobe ipip
+modprobe gre
+
+# 2020-01-19
+modprobe xpad
+
+# 2020-02-01
+modprobe sd_mod
+modprobe raid1
+modprobe raid0
+modprobe raid5
+
+# 2020-02-05
+modprobe nfs
